e3627bfb537a391e49e16aa0d6f56c8a35aff6e8
mshdsozk <mshdsozk8040+butbrunch@gmail.com>
2016-02-28 15:04:57 +0900

    コミックと活動中止のご挨拶を掲載

--- public_html/sp/js/index.js
+++ public_html/sp/js/index.js
@@ -7,3 +7,20 @@ $(function () {
         $(this).next().slideToggle();
     });
 });
+
+$(function () {
+    $('a[rel*=leanModal]').leanModal({
+        top: 50,
+        overlay: 0.7,
+    });
+});
+
+$(window).on('open:leanModal', function () {
+    console.log('open!');
+    $("body").css("overflow", "hidden");
+});
+
+$(window).on('close:leanModal', function () {
+    console.log('closed!');
+    $("body").css("overflow", "auto");
+});
